f9bf237dc34674e50d5096e16aa58bbacec08041
TheDukeVin <kevdu3435@gmail.com>
2016-06-26 20:37:21 -0700

    The Mandlebrot Set!
    Cool view of the mandlebrot set.

---
+++ Mandlebrot Set.py
@@ -0,0 +1,60 @@
+
+from __future__ import division
+import pygame,sys
+from pygame.locals import *
+pygame.init()
+
+windS = 600
+Max = 100
+
+name = 'Mandlebrot Set'
+
+def P(a,b,ca,cb):
+    return (a**2-b**2+ca,2*a*b+cb)
+
+def color(steps):
+    if steps == -1:
+        return (0,0,0)
+    if steps<Max/3:
+        dev = int(steps*(765/Max))
+        return (255-dev,dev,0)
+    if steps<2*Max/3:
+        dev = int(steps*(765/Max)-255)
+        return (0,255-dev,dev)
+    dev = int(steps*(765/Max)-510)
+    return (dev,0,255-dev)
+
+def mandle(ca,cb):
+    starta = 0
+    startb = 0
+    steps = 0
+    for i in range(0,Max):
+        if starta**2+startb**2>4:
+            return steps
+        newa,newb = P(starta,startb,ca,cb)
+        starta = newa
+        startb = newb
+        steps+=1
+    return -1
+
+def draw(cornerx,cornery,zoom):
+    pixels = pygame.PixelArray(window)
+    for i in range(0,windS):
+        pygame.display.update()
+        for j in range(0,windS):
+            pixels[i][j] = color(mandle(cornerx+i/zoom/windS,cornery+j/zoom/windS))
+
+def main():
+    global window
+    window = pygame.display.set_mode((windS,windS))
+    pygame.display.set_caption(name)
+    draw(-2,-2,1/4)
+    while True:
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+        pygame.display.update()
+
+if __name__ == "__main__":
+    main()
